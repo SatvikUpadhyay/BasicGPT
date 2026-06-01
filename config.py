001c3e9c9e5d2ed4d1bcbf25856e0f2f5a106bc4
SatvikUpadhyay <satvikupadhyay@gmail.com>
2026-06-01 15:20:53 -0500

    config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import torch
 import tiktoken
 
-d_model = 512
+d_model = 1024
 d_ff = d_model * 4
 
 enc = tiktoken.get_encoding("cl100k_base")
@@ -11,6 +11,6 @@ vocab_size = enc.n_vocab
 max_seq_length = 512
 batch_size = 8
 learning_rate = 1e-3
-num_epochs = 3
+num_epochs = 20
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
